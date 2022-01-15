45d5229f56549748954841d11b222e2b43b53d39
paul <paulbrowne2@gmail.com>
2022-01-15 13:26:47 +0000

    Added react js to static js files

--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -25,7 +25,7 @@ from app import views_orders
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/orders/', views_orders.orders),
     path('api/orders/<int:order_id>/', views_orders.order),
     path('', views.index),
