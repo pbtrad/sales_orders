c4a67326ce9a1a2f32641ef6ce5cdef8d4497736
paul <paulbrowne2@gmail.com>
2022-01-15 12:22:06 +0000

    Added basic views to app

--- sales/app/views.py
+++ sales/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, "index.html", context=context)
+
+def login_view(request):
+    context = {}
+    return render(request, "login.html", context=context)
--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -25,4 +25,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login', views.login_view),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
